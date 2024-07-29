4c9ee4aa25efb2ab983130d55a82c77ceedc3457
BeauX <fire-fly_7@hotmail.com>
2024-07-29 10:40:25 -0700

    schema ready

---
+++ models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const gameSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    genre: { type: String, required: true },
+    publisher: { type: String },
+    year: { type: Number },
+    havePlayed: { Boolean }
+});
+
+const Game = mongoose.model('Game', gameSchema)
+
+module.exports = Game
